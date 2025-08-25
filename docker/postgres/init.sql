41588e4eceb9685bb7e51a3072873c6fa3423fd5
Sambit99 <sambitkumar75@gmail.com>
2025-08-25 20:31:55 +0530

    Add init.sql to create stocks table in PostgreSQL

---
+++ docker/postgres/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS stocks (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    price INT NOT NULL,
+    company TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
